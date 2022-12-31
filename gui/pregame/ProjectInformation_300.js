24bc9b98d81538face7e202e9fb4ebeb8a66844d
Justus Avramenko <michaeldhafer@gmail.com>
2022-12-30 23:39:32 -0500

    More updates

--- gui/pregame/ProjectInformation_300.js
+++ gui/pregame/ProjectInformation_300.js
@@ -1,8 +1,8 @@
 g_ProjectInformation.productDescription.caption =
-	setStringTags(translate("300 Mod v0.0.01"), { "font": "sans-bold-16" }) + "\n\n" +
+	setStringTags(translate("300 Mod v0.27.0"), { "font": "sans-bold-16" }) + "\n\n" +
 	translate("Notice: This mod is under development and many features have not been added yet.");
 
-g_MainMenuItems[4].caption = translate("Atlas Scenario Editor");
+g_MainMenuItems[5].caption = translate("Atlas Scenario Editor");
 
 g_CommunityButtons[0].tooltip = translate("Click to open https://0ad.mod.io/300 in your web browser.");
 g_CommunityButtons[0].onPress = () => {
